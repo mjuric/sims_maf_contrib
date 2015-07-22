c4bfd9238ba564a9335530868e8a09cc5ad38e57
marcusbooyah <mbowyer@lsst-dev.ncsa.illinois.edu>
2015-07-22 00:25:49 -0500

    prep

---
+++ prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This script needs to be _sourced_ from travis.yml
+# so $PATH is set up correctly
+#
+
+set -e
+
+# keep the pkginfo cache database outside the conda-lsst dir
+# so Travis can cache it easily.
+mkdir -p pkginfo-cache
+
+# Prepare conda-lsst environment
+rm -rf conda-lsst
+git clone https://github.com/mjuric/conda-lsst
+cd conda-lsst
+./bin/bootstrap.sh
+export PATH="$PWD/bin:$PWD/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels http://eupsforge.net/conda/dev
+conda install lsst-apps
+conda install lsst-sims-maf
